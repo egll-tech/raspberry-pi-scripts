69e4fe84f335f03399583aba75c316ce5930b4c4
EAGLE <eduardo@egll.techn>
2020-08-16 21:45:20 -0400

    Solving no /etc/resolv.conf on reboot

--- scripts/pihole.sh
+++ scripts/pihole.sh
@@ -17,8 +17,11 @@ fi
 echo "Disabling current DNS server"
 sudo systemctl stop systemd-resolved
 sudo systemctl disable systemd-resolved &> /dev/null
-echo "Updating default DNS to 1.1.1.3"
-sudo sed -i -e 's/127.0.0.53/1.1.1.3/g' /etc/resolv.conf
+sudo mv /etc/resolv.conf /etc/resolv.conf.bak
+sudo cat > /etc/resolv.conf <<EOL
+nameserver 1.1.1.3
+nameserver 1.0.0.3
+EOL
 
 echo "Installing pihole"
 curl -sL https://raw.githubusercontent.com/pi-hole/docker-pi-hole/master/docker_run.sh | sudo -E bash -
